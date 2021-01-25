23d30b308e17df23785b3e49aacc48147620b0fd
gudbrant <61345734+gudbrant@users.noreply.github.com>
2021-01-25 10:49:26 +0800

    0.0.0

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,22 +1,12 @@
-import themes from './themes';
-
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
-    this.levels = ['', 'prairie', 'desert', 'arctic', 'mountain'];
   }
 
   init() {
     // TODO: add event listeners to gamePlay events
-    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
-    this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
-    this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
-    this.gamePlay.addNewGameListener(this.newGame.bind(this));
-    this.gamePlay.addSaveGameListener(this.saveGame.bind(this));
-    this.gamePlay.addLoadGameListener(this.loadGame.bind(this));
     // TODO: load saved stated from stateService
-    this.loadGame();
   }
 
   onCellClick(index) {
